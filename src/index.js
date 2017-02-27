ea6a42944c052a1e1d57104886be269a6c5299ba
rafrex <code@rafrex.com>
2017-02-27 11:41:25 -0800

    Update Object.defineProperty check

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 // adapted from https://github.com/WICG/EventListenerOptions/blob/gh-pages/explainer.md
 const detectPassiveEvents = {
   update() {
-    if (typeof window === 'object' && typeof window.addEventListener === 'function' && Object.defineProperty) {
+    if (
+      typeof window === 'object' &&
+      typeof window.addEventListener === 'function' &&
+      typeof Object.defineProperty === 'function'
+    ) {
       let passive = false;
       const options = Object.defineProperty({}, 'passive', {
         get() { passive = true; },
