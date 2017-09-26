b33b2723077d5ddce8060009e917caed7099a7b2
rafrex <code@rafrex.com>
2017-09-26 11:08:33 -0700

    Clean up no-op listener

--- src/index.js
+++ src/index.js
@@ -6,8 +6,13 @@ const detectPassiveEvents = {
       const options = Object.defineProperty({}, 'passive', {
         get() { passive = true; },
       });
-      window.addEventListener('test', () => {}, options);
-
+      // note: have to set and remove a no-op listener instead of null
+      // (which was used previously), becasue Edge v15 throws an error
+      // when providing a null callback.
+      // https://github.com/rafrex/detect-passive-events/pull/3
+      const noop = () => {};
+      window.addEventListener('testPassiveEventSupport', noop, options);
+      window.removeEventListener('testPassiveEventSupport', noop, options);
       detectPassiveEvents.hasSupport = passive;
     }
   },
