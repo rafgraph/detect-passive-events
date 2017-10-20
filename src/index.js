8cb3b2abdba416753d0101005ff94652e4b83ad4
Mateusz Burzyński <mateuszburzynski@gmail.com>
2017-10-20 11:20:15 +0200

    Exporting as named export bare test function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 // adapted from https://github.com/WICG/EventListenerOptions/blob/gh-pages/explainer.md
+export const testPassiveEventSupport = () => {
+  if (typeof window === 'undefined' || typeof window.addEventListener !== 'function') {
+    return;
+  }
+
+  let passive = false;
+
+  const options = Object.defineProperty({}, 'passive', {
+    get() { passive = true; },
+  });
+
+  // note: have to set and remove a no-op listener instead of null
+  // (which was used previously), becasue Edge v15 throws an error
+  // when providing a null callback.
+  // https://github.com/rafrex/detect-passive-events/pull/3
+  const noop = () => {};
+
+  window.addEventListener('testPassiveEventSupport', noop, options);
+  window.removeEventListener('testPassiveEventSupport', noop, options);
+
+  return passive
+}
+
 const detectPassiveEvents = {
   update() {
-    if (typeof window !== 'undefined' && typeof window.addEventListener === 'function') {
-      let passive = false;
-      const options = Object.defineProperty({}, 'passive', {
-        get() { passive = true; },
-      });
-      // note: have to set and remove a no-op listener instead of null
-      // (which was used previously), becasue Edge v15 throws an error
-      // when providing a null callback.
-      // https://github.com/rafrex/detect-passive-events/pull/3
-      const noop = () => {};
-      window.addEventListener('testPassiveEventSupport', noop, options);
-      window.removeEventListener('testPassiveEventSupport', noop, options);
-      detectPassiveEvents.hasSupport = passive;
-    }
+    detectPassiveEvents.hasSupport = testPassiveEventSupport();
   },
 };
 
